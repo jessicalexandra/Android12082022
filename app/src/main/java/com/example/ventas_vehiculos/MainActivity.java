9a4bea7da14f136163240df3eb55325d9d020a5f
Fredbear1 <elkinposada03@gmail.com>
2022-08-19 20:49:34 -0500

    1908

--- app/src/main/java/com/example/ventas_vehiculos/MainActivity.java
+++ app/src/main/java/com/example/ventas_vehiculos/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.ventas_vehiculos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void vehiculo (View view){
+        Intent intvehiculo= new Intent(this,VehiculoActivity.class);
+        startActivity(intvehiculo);
+
+    }
+
+    public void factura(View view){
+         Intent intfactura=new Intent( this, FacturaActivity.class);
+         startActivity(intfactura);
     }
 }
